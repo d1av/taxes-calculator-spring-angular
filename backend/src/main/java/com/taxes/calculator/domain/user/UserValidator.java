6f754aa23691124f0867becac140807275b46faa
d1av <davi4alves@gmail.com>
2023-02-12 02:56:51 -0300

    test: :test_tube: add password validation for user

--- backend/src/main/java/com/taxes/calculator/domain/user/UserValidator.java
+++ backend/src/main/java/com/taxes/calculator/domain/user/UserValidator.java
@@ -1,75 +1,62 @@
 package com.taxes.calculator.domain.user;
 
-import com.taxes.calculator.domain.utils.IdUtils;
+import java.util.Objects;
+
 import com.taxes.calculator.domain.validation.Error;
 import com.taxes.calculator.domain.validation.ValidationHandler;
 import com.taxes.calculator.domain.validation.Validator;
 
-import java.util.Objects;
-
 public class UserValidator extends Validator {
 
-    private static final int NAME_MAX_LENGTH = 200;
-    private static final int NAME_MIN_LENGTH = 1;
-    private static final int PASSWORD_MIN_LENGTH = 6;
-    private static final int PASSWORD_MAX_LENGTH = 20;
-    private final User user;
+	private static final int NAME_MAX_LENGTH = 200;
+	private static final int NAME_MIN_LENGTH = 1;
+	private static final int PASSWORD_MIN_LENGTH = 6;
+	private static final int PASSWORD_MAX_LENGTH = 20;
+	private final User user;
 
-    public UserValidator(final User user,
-                         final ValidationHandler aHandler) {
-        super(aHandler);
-        this.user = user;
-    }
+	public UserValidator(final User user, final ValidationHandler aHandler) {
+		super(aHandler);
+		this.user = user;
+	}
 
-    @Override
-    public void validate() {
-        checkNameConstraints();
-        checkPasswordConstraints();
-        checkIdConstraints();
-    }
+	@Override
+	public void validate() {
+		checkNameConstraints();
+		checkPasswordConstraints();
+	}
 
-    private void checkIdConstraints() {
-        final var id = this.user.getId().getValue();
-        if (Objects.isNull(id)) {
-            this.validationHandler().append(new Error("'id' should not be null"));
-        }
-        int idLength = IdUtils.uuid().length();
-        if(id.length()!= idLength){
-            this.validationHandler().append(new Error("'id' should be valid with %s".formatted(idLength)));
-        }
-    }
+	private void checkNameConstraints() {
+		final var name = this.user.getName();
+		if (Objects.isNull(name)) {
+			this.validationHandler().append(new Error("'name' should not be null"));
+		} else {
+			if (name.trim().isBlank()) {
+				this.validationHandler().append(new Error("'name' should not be empty"));
+			} else {
+				final int length = name.length();
+				if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
+					this.validationHandler().append(new Error(
+							"'name' must be between %s and %s characters".formatted(NAME_MIN_LENGTH, NAME_MAX_LENGTH)));
+				}
 
-    private void checkNameConstraints() {
-        final var name = this.user.getName();
-        if (Objects.isNull(name)) {
-            this.validationHandler().append(new Error("'name' should not be null"));
-        } else {
-            if (name.trim().isBlank()) {
-                this.validationHandler().append(new Error("'name' should not be empty"));
-            }
-            final int length = name.length();
-            if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
-                this.validationHandler()
-                        .append(new Error("'name' must be between %s and %s characters"
-                                .formatted(NAME_MIN_LENGTH, NAME_MAX_LENGTH)));
-            }
-        }
-    }
+			}
+		}
+	}
 
-    private void checkPasswordConstraints() {
-        final var password = this.user.getPassword();
-        if (Objects.isNull(password)) {
-            this.validationHandler().append(new Error("'password' should not be null"));
-        } else {
-            if (password.trim().isBlank()) {
-                this.validationHandler().append(new Error("'password' should not be empty"));
-            }
-            final int length = password.length();
-            if (length > PASSWORD_MAX_LENGTH || length < PASSWORD_MIN_LENGTH) {
-                this.validationHandler()
-                        .append(new Error("'password' must be between %s and %s characters"
-                                .formatted(PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH)));
-            }
-        }
-    }
+	private void checkPasswordConstraints() {
+		final var password = this.user.getPassword();
+		if (Objects.isNull(password)) {
+			this.validationHandler().append(new Error("'password' should not be null"));
+		} else {
+			if (password.trim().isBlank()) {
+				this.validationHandler().append(new Error("'password' should not be empty"));
+			} else {
+				final int length = password.length();
+				if (length > PASSWORD_MAX_LENGTH || length < PASSWORD_MIN_LENGTH) {
+					this.validationHandler().append(new Error("'password' must be between %s and %s characters"
+							.formatted(PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH)));
+				}
+			}
+		}
+	}
 }
--- backend/src/test/java/com/taxes/calculator/domain/user/UserTest.java
+++ backend/src/test/java/com/taxes/calculator/domain/user/UserTest.java
@@ -45,7 +45,7 @@ public class UserTest {
         final String expectedName = null;
         final String expectedPassword = Fixture.password(8);
         final Set<Role> expectedRoles = Set.of(Fixture.Roles.member());
-        final Boolean expectedActive = true;
+        final boolean expectedActive = true;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMesage = "'name' should not be null";
@@ -64,4 +64,137 @@ public class UserTest {
         Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
         Assertions.assertEquals(expectedErrorMesage, actualException.firstError().message());
     }
+    
+    
+    @Test
+    public void givenAInvalidEmptyName_whenCallsCreateUser_shouldReturnAnNotification() {
+    	// given
+    	final String expectedName = "";
+    	final String expectedPassword = Fixture.password(8);
+    	final Set<Role> expectedRoles = Set.of(Fixture.Roles.member());
+    	final boolean expectedActive = true;
+    	
+    	final var expectedErrorCount = 1;
+    	final var expectedErrorMesage = "'name' should not be empty";
+    	
+    	// when
+    	final var actualException =
+    			Assertions.assertThrows(
+    					NotificationException.class, () -> User.with(
+    							expectedName,
+    							expectedPassword,
+    							expectedActive,
+    							expectedRoles)
+    					);
+    	
+    	// then
+    	Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    	Assertions.assertEquals(expectedErrorMesage, actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidBlankName_whenCallsCreateUser_shouldReturnAnNotification() {
+    	// given
+    	final String expectedName = "    ";
+    	final String expectedPassword = Fixture.password(8);
+    	final Set<Role> expectedRoles = Set.of(Fixture.Roles.member());
+    	final boolean expectedActive = true;
+    	
+    	final var expectedErrorCount = 1;
+    	final var expectedErrorMesage = "'name' should not be empty";
+    	
+    	// when
+    	final var actualException =
+    			Assertions.assertThrows(
+    					NotificationException.class, () -> User.with(
+    							expectedName,
+    							expectedPassword,
+    							expectedActive,
+    							expectedRoles)
+    					);
+    	
+    	// then
+    	Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    	Assertions.assertEquals(expectedErrorMesage, actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidNullPassword_whenCallsCreateUser_shouldReturnAnNotification() {
+        // given
+        final String expectedName = Fixture.name();
+        final String expectedPassword = null;
+        final Set<Role> expectedRoles = Set.of(Fixture.Roles.member());
+        final boolean expectedActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMesage = "'password' should not be null";
+
+        // when
+        final var actualException =
+                Assertions.assertThrows(
+                        NotificationException.class, () -> User.with(
+                                expectedName,
+                                expectedPassword,
+                                expectedActive,
+                                expectedRoles)
+                );
+
+        // then
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMesage, actualException.firstError().message());
+    }
+    
+    
+    @Test
+    public void givenAInvalidEmptyPassword_whenCallsCreateUser_shouldReturnAnNotification() {
+    	// given
+    	final String expectedName = Fixture.name();
+    	final String expectedPassword = Fixture.password(22);
+    	final Set<Role> expectedRoles = Set.of(Fixture.Roles.member());
+    	final boolean expectedActive = true;
+    	
+    	final var expectedErrorCount = 1;
+    	final var expectedErrorMesage = "'password' must be between 6 and 20 characters";
+    	
+    	// when
+    	final var actualException =
+    			Assertions.assertThrows(
+    					NotificationException.class, () -> User.with(
+    							expectedName,
+    							expectedPassword,
+    							expectedActive,
+    							expectedRoles)
+    					);
+    	
+    	// then
+    	Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    	Assertions.assertEquals(expectedErrorMesage, actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidBlankPassword_whenCallsCreateUser_shouldReturnAnNotification() {
+    	// given
+    	final String expectedName = Fixture.name();
+    	final String expectedPassword = "   ";
+    	final Set<Role> expectedRoles = Set.of(Fixture.Roles.member());
+    	final boolean expectedActive = true;
+    	
+    	final var expectedErrorCount = 1;
+    	final var expectedErrorMesage = "'password' should not be empty";
+    	
+    	// when
+    	final var actualException =
+    			Assertions.assertThrows(
+    					NotificationException.class, () -> User.with(
+    							expectedName,
+    							expectedPassword,
+    							expectedActive,
+    							expectedRoles)
+    					);
+    	
+    	// then
+    	Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    	Assertions.assertEquals(expectedErrorMesage, actualException.firstError().message());
+    }
 }
+
