52882504d60c66a6ed80e0251c45dd0082f9df66
d1av <davi4alves@gmail.com>
2023-02-12 15:52:31 -0300

    test: :test_tube: add fixedTax null checkers

--- backend/src/test/java/com/taxes/calculator/domain/fixedtax/FixedTaxTest.java
+++ backend/src/test/java/com/taxes/calculator/domain/fixedtax/FixedTaxTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.taxes.calculator.Fixture;
+import com.taxes.calculator.domain.exceptions.NotificationException;
 import com.taxes.calculator.domain.user.User;
 
 class FixedTaxTest {
@@ -92,5 +93,303 @@ class FixedTaxTest {
 	Assertions.assertNotNull(actualTax.getCreatedAt());
 	Assertions.assertNotNull(actualTax.getUpdatedAt());
     }
+
+    @Test
+    public void givenAInvalidNullRegionalCouncil_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = null;
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'regionalCouncil' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+
+    @Test
+    public void givenAInvalidNullTaxOverWork_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = null;
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'taxOverWork' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+
+    @Test
+    public void givenAInvalidNullIncomeTax_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = null;
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'incomeTax' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+
+    @Test
+    public void givenAInvalidNullAccountant_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = null;
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'accountant' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidNullDentalShop_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = null;
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'dentalShop' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidNullTransport_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = null;
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'transport' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidNullFood_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = null;
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'food' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidNullEducation_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = null;
+	final BigDecimal expectedOtherFixedCosts = Fixture.bigDecimal(4);
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'education' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+    
+    @Test
+    public void givenAInvalidNullOtherFixedCosts_whenCreateNewFixedTax_shouldReturnNotification() {
+	// given
+	final BigDecimal expectedRegionalCouncil = Fixture.bigDecimal(4);
+	final BigDecimal expectedTaxOverWork = Fixture.bigDecimal(4);
+	final BigDecimal expectedIncomeTax = Fixture.bigDecimal(4);
+	final BigDecimal expectedAccountant = Fixture.bigDecimal(4);
+	final BigDecimal expectedDentalShop = Fixture.bigDecimal(4);
+	final BigDecimal expectedTransport = Fixture.bigDecimal(4);
+	final BigDecimal expectedFood = Fixture.bigDecimal(4);
+	final BigDecimal expectedEducation = Fixture.bigDecimal(4);
+	final BigDecimal expectedOtherFixedCosts = null;
+	final User expectedUser = null;
+
+	final var expectedErrorSize = 1;
+	final var expectedErrorMessage = "'otherFixedCosts' should not be null";
+
+	// when
+	final var actualException = Assertions.assertThrows(
+		NotificationException.class,
+		() -> FixedTax.with(expectedRegionalCouncil,
+			expectedTaxOverWork, expectedIncomeTax,
+			expectedAccountant, expectedDentalShop,
+			expectedTransport, expectedFood, expectedEducation,
+			expectedOtherFixedCosts, expectedUser));
+
+	// then
+	Assertions.assertEquals(expectedErrorSize,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+    }
+
 
 }
