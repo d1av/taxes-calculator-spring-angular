f6f11d8db1f9661d2f4c2b417348ff86d0bae286
d1av <davi4alves@gmail.com>
2023-02-13 21:15:43 -0300

    feat: :sparkles: add hourvalue factory methods

--- backend/src/main/java/com/taxes/calculator/domain/hourvalue/HourValue.java
+++ backend/src/main/java/com/taxes/calculator/domain/hourvalue/HourValue.java
@@ -2,8 +2,10 @@ package com.taxes.calculator.domain.hourvalue;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.Objects;
 
 import com.taxes.calculator.domain.AggregateRoot;
+import com.taxes.calculator.domain.utils.InstantUtils;
 import com.taxes.calculator.domain.validation.ValidationHandler;
 
 public class HourValue extends AggregateRoot<HourValueID> {
@@ -26,31 +28,71 @@ public class HourValue extends AggregateRoot<HourValueID> {
     }
 
     @Override
-    public void validate(ValidationHandler handler) {
-	// TODO Auto-generated method stub
+    public void validate(ValidationHandler aHandler) {
+	new HourValueValidator(this, aHandler);
+    }
 
+    public static HourValue with(final BigDecimal aExpectedSalary,
+	    final BigDecimal aHourValue, final BigDecimal aDaysOfWork) {
+	final var anId = HourValueID.unique();
+	final var now = InstantUtils.now();
+	return new HourValue(anId, aExpectedSalary, aHourValue, aDaysOfWork,
+		now, now);
+    }
+
+    public static HourValue with(final HourValue aHourValue) {
+	return new HourValue(aHourValue.id, aHourValue.expectedSalary,
+		aHourValue.hourValue, aHourValue.daysOfWork,
+		aHourValue.createdAt, aHourValue.getUpdatedAt());
+    }
+
+    public HourValueID getId() {
+	return id;
     }
 
     public BigDecimal getExpectedSalary() {
-        return expectedSalary;
+	return expectedSalary;
     }
 
     public BigDecimal getHourValue() {
-        return hourValue;
+	return hourValue;
     }
 
     public BigDecimal getDaysOfWork() {
-        return daysOfWork;
+	return daysOfWork;
     }
 
     public Instant getCreatedAt() {
-        return createdAt;
+	return createdAt;
     }
 
     public Instant getUpdatedAt() {
-        return updatedAt;
+	return updatedAt;
     }
-    
-    
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result + Objects.hash(createdAt, daysOfWork,
+		expectedSalary, hourValue, updatedAt);
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	HourValue other = (HourValue) obj;
+	return Objects.equals(createdAt, other.createdAt)
+		&& Objects.equals(daysOfWork, other.daysOfWork)
+		&& Objects.equals(expectedSalary, other.expectedSalary)
+		&& Objects.equals(hourValue, other.hourValue)
+		&& Objects.equals(updatedAt, other.updatedAt);
+    }
 
 }
