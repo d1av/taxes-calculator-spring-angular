649f3084368aa40e69aab8ecd92c6bf2b41d620b
d1av <davi4alves@gmail.com>
2023-02-14 01:57:11 -0300

    test: :test_tube: user creation and validation

--- backend/src/test/java/com/taxes/calculator/application/user/create/CreateUserUseCaseTest.java
+++ backend/src/test/java/com/taxes/calculator/application/user/create/CreateUserUseCaseTest.java
@@ -4,6 +4,7 @@ import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -19,6 +20,7 @@ import org.mockito.Mock;
 import com.taxes.calculator.Fixture;
 import com.taxes.calculator.application.UseCaseTest;
 import com.taxes.calculator.application.utils.MapperUtils;
+import com.taxes.calculator.domain.exceptions.NotificationException;
 import com.taxes.calculator.domain.role.Role;
 import com.taxes.calculator.domain.role.RoleGateway;
 import com.taxes.calculator.domain.role.RoleID;
@@ -106,6 +108,229 @@ class CreateUserUseCaseTest extends UseCaseTest {
 		&& Objects.equals(expectedIsActive, aUser.getActive())
 		&& Objects.nonNull(aUser.getCreatedAt())
 		&& Objects.nonNull(aUser.getUpdatedAt())));
+    }
+
+    @Test
+    void givenAInvalidNullNameCommand_whenCreateUser_shouldReturnException() {
+	// given
+	final String expectedName = null;
+	final var expectedPassword = Fixture.password(6);
+	final var expectedIsActive = true;
+	final var expectedRoles = Set.of(Fixture.Roles.guest(),
+		Fixture.Roles.member());
+
+	final var expectedErrorCount = 1;
+	final var expectedErrorMessage = "'name' should not be null";
+
+	final var aCommand = CreateUserCommand.with(expectedName,
+		expectedPassword, expectedIsActive, expectedRoles);
+
+	final var mappedRoles = MapperUtils.toID(expectedRoles,
+		Role::getId);
+
+	when(roleGateway.existsByIds(any())).thenReturn(mappedRoles);
+
+	// when
+
+	final var actualException = Assertions
+		.assertThrows(NotificationException.class, () -> {
+		    useCase.execute(aCommand);
+		});
+
+	// then
+	Assertions.assertNotNull(actualException);
+	Assertions.assertEquals(expectedErrorCount,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+
+	verify(userGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAInvalidNullPasswordCommand_whenCreateUser_shouldReturnException() {
+	// given
+	final var expectedName = Fixture.name();
+	final String expectedPassword = null;
+	final var expectedIsActive = true;
+	final var expectedRoles = Set.of(Fixture.Roles.guest(),
+		Fixture.Roles.member());
+
+	final var expectedErrorCount = 1;
+	final var expectedErrorMessage = "'password' should not be null";
+
+	final var aCommand = CreateUserCommand.with(expectedName,
+		expectedPassword, expectedIsActive, expectedRoles);
+
+	final var mappedRoles = MapperUtils.toID(expectedRoles,
+		Role::getId);
+
+	when(roleGateway.existsByIds(any())).thenReturn(mappedRoles);
+
+	// when
+
+	final var actualException = Assertions
+		.assertThrows(NotificationException.class, () -> {
+		    useCase.execute(aCommand);
+		});
+
+	// then
+	Assertions.assertNotNull(actualException);
+	Assertions.assertEquals(expectedErrorCount,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+
+	verify(userGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAInvalidMinLengthPasswordCommand_whenCreateUser_shouldReturnException() {
+	// given
+	final var expectedName = Fixture.name();
+	final String expectedPassword = Fixture.text(4);
+	final var expectedIsActive = true;
+	final var expectedRoles = Set.of(Fixture.Roles.guest(),
+		Fixture.Roles.member());
+
+	final var expectedErrorCount = 1;
+	final var expectedErrorMessage = "'password' must be between 6 and 20 characters";
+
+	final var aCommand = CreateUserCommand.with(expectedName,
+		expectedPassword, expectedIsActive, expectedRoles);
+
+	final var mappedRoles = MapperUtils.toID(expectedRoles,
+		Role::getId);
+
+	when(roleGateway.existsByIds(any())).thenReturn(mappedRoles);
+
+	// when
+
+	final var actualException = Assertions
+		.assertThrows(NotificationException.class, () -> {
+		    useCase.execute(aCommand);
+		});
+
+	// then
+	Assertions.assertNotNull(actualException);
+	Assertions.assertEquals(expectedErrorCount,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+
+	verify(userGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAInvalidMaxLengthPasswordCommand_whenCreateUser_shouldReturnException() {
+	// given
+	final var expectedName = Fixture.name();
+	final String expectedPassword = Fixture.text(21);
+	final var expectedIsActive = true;
+	final var expectedRoles = Set.of(Fixture.Roles.guest(),
+		Fixture.Roles.member());
+
+	final var expectedErrorCount = 1;
+	final var expectedErrorMessage = "'password' must be between 6 and 20 characters";
+
+	final var aCommand = CreateUserCommand.with(expectedName,
+		expectedPassword, expectedIsActive, expectedRoles);
+
+	final var mappedRoles = MapperUtils.toID(expectedRoles,
+		Role::getId);
+
+	when(roleGateway.existsByIds(any())).thenReturn(mappedRoles);
+
+	// when
+
+	final var actualException = Assertions
+		.assertThrows(NotificationException.class, () -> {
+		    useCase.execute(aCommand);
+		});
+
+	// then
+	Assertions.assertNotNull(actualException);
+	Assertions.assertEquals(expectedErrorCount,
+		actualException.getErrors().size());
+	Assertions.assertEquals(expectedErrorMessage,
+		actualException.firstError().message());
+
+	verify(userGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAInvalidNullActiveCommand_whenCreateUser_shouldReturnActiveUser() {
+	// given
+	final var expectedName = Fixture.name();
+	final var expectedPassword = Fixture.text(8);
+	final Boolean expectedIsActive = null;
+	final var expectedRoles = Set.of(Fixture.Roles.guest(),
+		Fixture.Roles.member());
+
+	final var expectedErrorCount = 1;
+	final var expectedErrorMessage = "'password' must be between 6 and 20 characters";
+
+	final var aCommand = CreateUserCommand.with(expectedName,
+		expectedPassword, expectedIsActive, expectedRoles);
+
+	final var mappedRoles = MapperUtils.toID(expectedRoles,
+		Role::getId);
+
+	when(userGateway.create(any())).thenAnswer(returnsFirstArg());
+
+	when(roleGateway.existsByIds(eq(mappedRoles)))
+		.thenReturn(mappedRoles);
+
+	// when
+	final var actualOutput = useCase.execute(aCommand);
+
+	// then
+	Assertions.assertNotNull(actualOutput);
+
+	verify(userGateway).create(argThat(aUser -> Objects
+		.nonNull(aUser.getId())
+		&& Objects.equals(expectedName, aUser.getName())
+		&& Objects.equals(expectedPassword,
+			aUser.getPassword())
+		&& Objects.equals(true, aUser.getActive())
+		&& Objects.nonNull(aUser.getCreatedAt())
+		&& Objects.nonNull(aUser.getUpdatedAt())));
+    }
+
+    @Test
+    void givenAEmptyRoleCommand_whenCreateUser_shouldReturnUser() {
+	// given
+	final var expectedName = Fixture.name();
+	final var expectedPassword = Fixture.password(10);
+	final var expectedIsActive = true;
+	final var expectedRoles = Set.<Role>of();
+
+	final var expectedErrorCount = 1;
+	final var expectedErrorMessage = "'password' should not be null";
+
+	final var aCommand = CreateUserCommand.with(expectedName,
+		expectedPassword, expectedIsActive, expectedRoles);
+
+	final var mappedRoles = MapperUtils.toID(expectedRoles,
+		Role::getId);
+
+	
+	when(userGateway.create(any())).thenAnswer(returnsFirstArg());
+	
+	// when
+		final var actualOutput = useCase.execute(aCommand);
+
+		// then
+		Assertions.assertNotNull(actualOutput);
+
+		verify(userGateway).create(argThat(aUser -> Objects
+			.nonNull(aUser.getId())
+			&& Objects.equals(expectedName, aUser.getName())
+			&& Objects.equals(expectedPassword,
+				aUser.getPassword())
+			&& Objects.equals(true, aUser.getActive())
+			&& Objects.nonNull(aUser.getCreatedAt())
+			&& Objects.nonNull(aUser.getUpdatedAt())));
     }
 
 }
