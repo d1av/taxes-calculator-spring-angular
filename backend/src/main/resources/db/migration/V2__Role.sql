641cf02d7f69a6b5e3cc9c0c2c04b36779faba9b
d1av <davi4alves@gmail.com>
2023-02-14 22:42:26 -0300

    feat: :sparkles: add roles migration

--- backend/src/main/resources/db/migration/V2__Role.sql
+++ backend/src/main/resources/db/migration/V2__Role.sql
@@ -1,2 +1,8 @@
-CREATE TABLE role(
+CREATE TABLE roles(
+	id CHAR(32) NOT NULL PRIMARY KEY,
+	authority VARCHAR(60) NOT NULL,
+    user_id CHAR(32) NULL,
+	created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    CONSTRAINT fk_user_role FOREIGN KEY (user_id) REFERENCES users (id)  
 );
