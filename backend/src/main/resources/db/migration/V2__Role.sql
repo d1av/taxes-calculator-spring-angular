fba6571451d83ffc774fe034aae68e71a85a8b97
d1av <davi4alves@gmail.com>
2023-02-20 17:42:54 -0300

    fix: :bug: migration was wrong

--- backend/src/main/resources/db/migration/V2__Role.sql
+++ backend/src/main/resources/db/migration/V2__Role.sql
@@ -1,8 +1,6 @@
 CREATE TABLE roles(
 	id CHAR(32) NOT NULL PRIMARY KEY,
 	authority VARCHAR(60) NOT NULL UNIQUE,
-    user_id CHAR(32) NULL,
 	created_at DATETIME(6) NOT NULL,
-    updated_at DATETIME(6) NOT NULL,
-    CONSTRAINT fk_user_role FOREIGN KEY (user_id) REFERENCES users (id)  
+    updated_at DATETIME(6) NOT NULL 
 );
