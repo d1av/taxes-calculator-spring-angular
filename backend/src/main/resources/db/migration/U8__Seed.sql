39afc1354d1340f95788b23d1bb4c147f1aa682e
d1av <davi4alves@gmail.com>
2023-04-02 14:54:54 -0300

    feat: :sparkles: add new database seed for user: usuario, pass: usuario

---
+++ backend/src/main/resources/db/migration/U8__Seed.sql
@@ -0,0 +1,21 @@
+DELETE FROM `users_fixedtaxes`;
+
+DELETE FROM `users_hourvalues`;
+
+DELETE FROM `users_roles`; 
+
+DELETE FROM `users_variabletaxes`;
+
+DELETE FROM `total_taxes`;
+
+DELETE FROM `users`;
+
+DELETE FROM `roles`; 
+
+DELETE FROM `fixed_taxes`; 
+
+DELETE FROM `hour_values`;
+
+DELETE FROM `variable_taxes`;
+ 
+ 
